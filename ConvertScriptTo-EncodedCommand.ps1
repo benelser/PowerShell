7b13be03d91edf6d3d86914f399da7a709374180
Benjamin Elser <Benjamin.elser18@gmail.com>
2019-10-11 09:57:27 -0500

    Create ConvertScriptTo-EncodedCommand.ps1

---
+++ ConvertScriptTo-EncodedCommand.ps1
@@ -0,0 +1,27 @@
+using namespace System.IO
+using namespace System.Text
+
+<#
+Great for creating download cradle or using to create scheduled tasks using the -encodedCommand param
+#>
+
+param (
+
+       [string] $FilePath
+)
+
+    try {
+
+       $script = [File]::ReadAllText($FilePath, [Encoding]::UTF8);
+       $ByteArray = [Encoding]::Unicode.GetBytes($script)
+       $encodedCommand = [Convert]::ToBase64String($ByteArray);
+       $encodedCommand | Out-File "$env:USERPROFILE\Desktop\EncodedCommand.txt"
+    }
+
+    catch {
+
+       throw "Something went wrong trying to convert $FilePath"
+       break
+    }
+
+Write-host -noNewline "*****" -ForegroundColor Yellow; Write-Host " Your EncodedCommand is ready in $env:USERPROFILE\Work Folders\Desktop\EncodedCommand.txt " -NoNewline -ForegroundColor Green; Write-host "*****`n`n" -ForegroundColor Yellow;
