6918dce32234f8c187e3e20aa5b9d0336647021c
Benjamin Elser <Benjamin.elser18@gmail.com>
2021-04-09 12:05:51 -0500

    Create PipelineMagic.ps1

---
+++ PipelineMagic.ps1
@@ -0,0 +1,23 @@
+$list = @(1,2,3,3)
+
+function Get-AllThrees {
+    param (
+        # This is the contect of the entity being processed 
+        [Parameter(ValueFromPipeline=$true)]
+        [object]$num
+    )
+    # This executes only once // set up stuff
+    Begin {
+       [System.Collections.ArrayList]$nums = @()
+    }
+    # This is essentially a implied foreach where we are writing context on 1 object instead of spelling out foreach
+    Process {
+        if ($num -eq 3) {
+            [void]$nums.Add($num)
+        }
+    }
+    # Executes once // tear down stuff
+    End {
+       $nums
+    }
+}
